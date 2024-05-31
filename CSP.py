e378f03e73a2037c251c11bc85c608ebe3a905f3
Rory Arnone-Wheat <kingjackie@live.com>
2024-05-31 10:17:25 -0700

    added cvs file reader

--- CSP.py
+++ CSP.py
@@ -1,1 +1,10 @@
+import pandas
 import csv
+csvFile = pandas.read_csv('CSPData.csv')
+print(csvFile)
+
+
+with open('CSPData.csv', mode ='r')as file:
+    csvFile2 = csv.reader(file)
+    for lines in csvFile2:
+        print(lines)
